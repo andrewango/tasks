751ae241592132869abdc07f018d50209fb06a38
ANDREW NGO <ango@udel.edu>
2023-03-12 17:37:56 -0400

    Solved GiveAttempts component

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
 export function EditMode(): JSX.Element {
+    const [inEdit, setInEdit] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
     return (
         <div>
             <h3>Edit Mode</h3>
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [numRemaining, setNumRemaining] = useState<number>(3);
+    const [numRequesting, setNumRequesting] = useState<number>(0);
+
+    function updateNumRequesting(event: React.ChangeEvent<HTMLInputElement>) {
+        const convertedInt = parseInt(event.target.value);
+        Number.isNaN(convertedInt)
+            ? setNumRequesting(0)
+            : setNumRequesting(convertedInt);
+    }
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>Attempts left: {numRemaining}.</div>
+            <div>
+                <Form.Group controlId="RequestAttemptsForm">
+                    <Form.Label>
+                        Type in your number of requested attempts:
+                    </Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={numRequesting}
+                        onChange={updateNumRequesting}
+                    />
+                </Form.Group>
+            </div>
+            <div>
+                <Button
+                    onClick={() => setNumRemaining(numRemaining - 1)}
+                    disabled={numRemaining === 0 ? true : false}
+                >
+                    use
+                </Button>
+                <Button
+                    onClick={() =>
+                        setNumRemaining(numRemaining + numRequesting)
+                    }
+                >
+                    gain
+                </Button>
+            </div>
         </div>
     );
 }
